21b10893e94406335b02b91db80bd226a9c248a5
saiprasad8310 <saiprasadrampure@gmail.com>
2025-03-03 19:20:35 +0530

    Delete p4.py

--- p4.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import seaborn as sns  
-
-plt.figure(figsize=(12,6))  
-sns.lineplot(data=df, x="Year", y="Consumption_MWh", hue="Energy_Type")  
-plt.title("Renewable vs. Non-Renewable Energy Usage Over Time")  
-plt.show()
-
-
